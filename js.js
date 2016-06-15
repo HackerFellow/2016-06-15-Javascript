b4a5b545efc285de594fd41cdd4aa00f005e4ef3
Mark Furland <markfurland@gmail.com>
2016-06-15 10:55:22 -0400

    Basic temperature conversion using jquery

---
+++ js.js
@@ -0,0 +1,13 @@
+function convertToCelsius(f){
+	return (f-32)*5/9;
+}
+function convertTemp(temp,targetUnit){
+	return targetUnit=="C"|targetUnit=="c" ? temp*9/5+32 : (temp-32)*5/9;
+}
+
+$( "#c" ).bind( "click", function() {
+	$("#result").text(convertTemp($("#input").val(),"f"));
+});
+$( "#f" ).bind( "click", function() {
+	$("#result").text(convertTemp($("#input").val(),"c"));
+});
