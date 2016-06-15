0da337d7856da79f5bf267c47b583cf7af40747c
Mark Furland <markfurland@gmail.com>
2016-06-15 11:17:30 -0400

    Validation of input

--- js.js
+++ js.js
@@ -2,6 +2,10 @@ function convertToCelsius(f){
 	return (f-32)*5/9;
 }
 function convertTemp(temp,targetUnit){
+	if(temp === ""){
+		alert("try again");
+		return;
+	}
 	return targetUnit=="C"|targetUnit=="c" ? temp*9/5+32 : (temp-32)*5/9;
 }
 
